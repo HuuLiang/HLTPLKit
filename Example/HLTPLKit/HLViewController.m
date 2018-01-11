46c6a385d3d21e95d848c9ca20fc24b0bbf39f96
Liang's Mac <757437150@qq.com>
2018-01-11 11:35:42 +0800

    Initial commit

---
+++ Example/HLTPLKit/HLViewController.m
@@ -0,0 +1,29 @@
+//
+//  HLViewController.m
+//  HLTPLKit
+//
+//  Created by 757437150@qq.com on 01/11/2018.
+//  Copyright (c) 2018 757437150@qq.com. All rights reserved.
+//
+
+#import "HLViewController.h"
+
+@interface HLViewController ()
+
+@end
+
+@implementation HLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
